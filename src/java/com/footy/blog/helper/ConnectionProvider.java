6852ad2e9298ec40d0a01bc60fe97245c7372089
Shubhadip Chand <110151805+shubhadipchand@users.noreply.github.com>
2024-08-20 22:50:26 +0530

    Update ConnectionProvider.java 11

--- src/java/com/footy/blog/helper/ConnectionProvider.java
+++ src/java/com/footy/blog/helper/ConnectionProvider.java
@@ -19,11 +19,11 @@ public class ConnectionProvider {
                 Class.forName("com.mysql.cj.jdbc.Driver");
 
                 // Fetch connection details from environment variables
-                String host = System.getenv("DB_HOST");
-                int port = System.getenv("DB_PORT");
-                String databaseName = System.getenv("DB_NAME");
-                String user = System.getenv("DB_USER");
-                String password = System.getenv("DB_PASSWORD");
+                String host = System.get_env("DB_HOST");
+                int port = System.get_env("DB_PORT");
+                String databaseName = System.get_env("DB_NAME");
+                String user = System.get_env("DB_USER");
+                String password = System.get_env("DB_PASSWORD");
 
                 // Construct the database URL with SSL mode enabled
                 String url = "jdbc:mysql://" + host + ":" + port + "/" + databaseName + "?useSSL=true&requireSSL=true";
