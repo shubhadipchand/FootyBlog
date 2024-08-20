3224468b4a29297adba1402db6c82d018e75c888
Shubhadip Chand <110151805+shubhadipchand@users.noreply.github.com>
2024-08-21 00:10:54 +0530

    Update ConnectionProvider.java 18

--- src/java/com/footy/blog/helper/ConnectionProvider.java
+++ src/java/com/footy/blog/helper/ConnectionProvider.java
@@ -7,6 +7,7 @@ package com.footy.blog.helper;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Locale;
 
 public class ConnectionProvider {
 
@@ -18,15 +19,15 @@ public class ConnectionProvider {
                 // Load the MySQL driver class
                 Class.forName("com.mysql.cj.jdbc.Driver");
 
-                // Fetch connection details (hardcoded or environment variables)
-                String host = "footyblog-shubhadipchand-11fb.b.aivencloud.com";
-                int port = 22785;
-                String databaseName = "defaultdb";
-                String user = "avnadmin";
+                // Fetch connection details from environment variables
+                String host = System.getenv("DB_HOST");
+                String port = System.getenv("DB_PORT");
+                String databaseName = System.getenv("DB_NAME");
+                String user = System.getenv("DB_USER");
                 String password = System.getenv("DB_PASSWORD");
 
                 // Construct the database URL with SSL mode enabled
-                String url = "jdbc:mysql://" + host + ":" + port + "/" + databaseName + "?useSSL=true&requireSSL=true";
+                String url = "jdbc:mysql://" + host + ":" + port + "/" + databaseName + "?sslmode=require";
 
                 // Create the connection
                 con = DriverManager.getConnection(url, user, password);
